af8ab050b3963e2fed1a07086a7fa35c54fa9115
masakinihirota <masakinihirota@gmail.com>
2024-08-02 02:11:07 +0900

    追加

--- src/app/(unauth)/unauth/layout.tsx
+++ src/app/(unauth)/unauth/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div style={{ border: '1px solid #ccc', padding: '20px' }}>
+    <div style={{ border: "1px solid #ccc", padding: "20px" }}>
       <h2>Layout Header</h2>
       {children}
       <h2>Layout Footer</h2>
