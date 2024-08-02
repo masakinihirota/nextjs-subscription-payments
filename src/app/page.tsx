977c931729c584bb3fd03fea3c94ce47b2437293
masakinihirota <masakinihirota@gmail.com>
2024-08-03 04:18:20 +0900

    キャッチフレーズ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+// TOP PAGE 静的ページ
 export default function Page() {
   return (
     <>
-      TOP PAGE
+      {/* キャッチフレーズ */}
+      昨日僕が感動したことを、今日の君はまだ知らない。
       <br />
       {/* unauthへのリンクを作成 */}
+      {/* これはメイン画面に移動する用*/}
       <a href="/unauth">unauth</a>
     </>
   );
