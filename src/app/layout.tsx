2f83746dbcd543ae7ababb4458a8476a8ef9832c
masakinihirota <masakinihirota@gmail.com>
2024-08-02 19:33:56 +0900

    add dropdown-menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import { Metadata } from "next";
 import { PropsWithChildren, Suspense } from "react";
+import { ThemeProvider } from 'next-themes';
 
-import Footer from "@/components/ui/Footer";
-import Navbar from "@/components/ui/Navbar";
-import { Toaster } from "@/components/ui/Toasts/toaster";
-import { getURL } from "@/utils/helpers";
-import "@/styles/main.css";
+import Footer from '@/components/ui/Footer';
+import Navbar from '@/components/ui/Navbar';
+import { Toaster } from '@/components/ui/Toasts/toaster';
+import { getURL } from '@/utils/helpers';
+import '@/styles/main.css';
 
-const title = "Next.js Subscription Starter";
-const description = "Brought to you by Vercel, Stripe, and Supabase.";
+const title = 'Next.js Subscription Starter';
+const description = 'Brought to you by Vercel, Stripe, and Supabase.';
 
 export const metadata: Metadata = {
   metadataBase: new URL(getURL()),
@@ -16,25 +17,27 @@ export const metadata: Metadata = {
   description: description,
   openGraph: {
     title: title,
-    description: description,
-  },
+    description: description
+  }
 };
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
+    <html lang="ja" suppressHydrationWarning>
       <body className="bg-black">
-        <Navbar />
-        <main
-          id="skip"
-          className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
-        >
-          {children}
-        </main>
-        <Footer />
-        <Suspense>
-          <Toaster />
-        </Suspense>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          <main
+            id="skip"
+            className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
+          >
+            {children}
+          </main>
+          <Footer />
+          <Suspense>
+            <Toaster />
+          </Suspense>
+        </ThemeProvider>
       </body>
     </html>
   );
