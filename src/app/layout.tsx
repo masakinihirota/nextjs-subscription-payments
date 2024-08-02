42a26470c93e94a539e2547c5cfe4d897962d22e
masakinihirota <masakinihirota@gmail.com>
2024-08-02 19:34:19 +0900

    dropdown-menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import { PropsWithChildren, Suspense } from "react";
-import { ThemeProvider } from 'next-themes';
 
-import Footer from '@/components/ui/Footer';
-import Navbar from '@/components/ui/Navbar';
-import { Toaster } from '@/components/ui/Toasts/toaster';
-import { getURL } from '@/utils/helpers';
-import '@/styles/main.css';
+import Footer from "@/components/ui/Footer";
+import Navbar from "@/components/ui/Navbar";
+import { Toaster } from "@/components/ui/Toasts/toaster";
+import { getURL } from "@/utils/helpers";
+import "@/styles/main.css";
 
-const title = 'Next.js Subscription Starter';
-const description = 'Brought to you by Vercel, Stripe, and Supabase.';
+const title = "Next.js Subscription Starter";
+const description = "Brought to you by Vercel, Stripe, and Supabase.";
 
 export const metadata: Metadata = {
   metadataBase: new URL(getURL()),
@@ -17,8 +17,8 @@ export const metadata: Metadata = {
   description: description,
   openGraph: {
     title: title,
-    description: description
-  }
+    description: description,
+  },
 };
 
 export default async function RootLayout({ children }: PropsWithChildren) {
